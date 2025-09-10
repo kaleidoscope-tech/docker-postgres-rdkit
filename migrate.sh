85e90c9d3e5fbc0d71148310bc5690be56baac46
Jeff Chen <hello@jeff.yt>
2025-09-10 10:11:48 -0600

    z

--- migrate.sh
+++ migrate.sh
@@ -6,19 +6,27 @@ set -e
 echo "Building Postgres+RDKit Docker image..."
 docker build -f Dockerfile  --build-arg postgres_image_version="13-bullseye" --build-arg postgres_version=13 --build-arg rdkit_git_ref=Release_2021_03_5 --tag postgres:13-rdkit_2021_03_5 .
 
-# If a data dir is specified, use it; otherwise, use the default
-if [ -z "$1" ]; then
-  POSTGRES_DATA_DIR="$HOME/kaleidoscope/db/postgres_data"
-else
-  POSTGRES_DATA_DIR="$1"
+# Require data dir as an argument
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 /path/to/postgres_data_directory"
+  exit 1
 fi
+
+POSTGRES_DATA_DIR=$1
 
 echo "Using Postgres data directory: $POSTGRES_DATA_DIR"
 
 # Stop and any existing Postgres container (assuming listening to 5432)
 containers=$(docker ps -a | grep ':5432->5432' | awk '{print $1}')
 if [ ! -z "$containers" ]; then
-  echo "Stopping and removing existing Postgres containers..."
+  echo "Found existing Postgres containers: $containers"
+  echo "Stop existing Postgres containers? (y/n)"
+  read answer
+  if [ "$answer" != "y" ]; then
+    echo "Aborting."
+    exit 1
+  fi
+  
   for container in $containers; do
     docker stop $container
   done
